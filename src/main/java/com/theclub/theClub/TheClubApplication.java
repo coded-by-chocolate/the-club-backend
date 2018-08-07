c3147b2a4e1fd19a425b3e362cd411e94c139a08
David Halpin <david.halpin@infocarehealth.com>
2018-08-07 20:30:22 +0100

    initial commit

---
+++ src/main/java/com/theclub/theClub/TheClubApplication.java
@@ -0,0 +1,12 @@
+package com.theclub.theClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheClubApplication.class, args);
+	}
+}
